1af26e384bc66238e5b9468877957155e27607cf
AngusBlomley <125128100+AngusBlomley@users.noreply.github.com>
2024-05-17 22:03:10 +0100

    carousel update

--- src/components/index/work.js
+++ src/components/index/work.js
@@ -5,6 +5,7 @@ import 'aos/dist/aos.css';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 import '../../app/globals.css';
+import Link from 'next/link';
 
 function Work({ isDarkMode }) {
     const [showMainCarousel, setShowMainCarousel] = useState(false);
@@ -31,12 +32,11 @@ function Work({ isDarkMode }) {
     const color = isDarkMode ? 'var(--foreground-color-dark)' : 'var(--foreground-color-light)';
 
     const images = [
-        '/images/work/stringbox.png',
-        '/images/work/cst.jpg',
-        '/images/work/befirst.png',
-        '/images/work/misuzu.png',
-        '/images/work/github.png',
-        // Add paths to all 10 images,
+        { src: '/images/work/stringbox.png', link: '/more-info/stringbox', title: 'String Box Project' },
+        { src: '/images/work/cst.jpg', link: '/more-info/cst', title: 'Final Major Project' },
+        { src: '/images/work/befirst.png', link: '/more-info/befirst', title: 'Be First Campaign' },
+        { src: '/images/work/misuzu.png', link: '/more-info/misuzu', title: 'Misuzu Collaboration' },
+        // Add paths, links, and titles for all 10 images,
     ];
 
     const handleSlideshowChange = (index) => {
@@ -46,31 +46,38 @@ function Work({ isDarkMode }) {
     };
 
     return (
-        <section id="work" style={{ backgroundColor, color }} className="relative flex flex-col justify-center items-center min-h-screen z-10 duration-200 overflow-hidden">
-            <div className="flex justify-center items-center w-full h-full">
-                <div className="w-full h-full items-center">
+        <section id="work" style={{ backgroundColor, color }} className="pt-16 relative min-h-screen flex justify-center items-center z-10 overflow-hidden">
+            <div className="flex justify-center items-center w-full">
+                <div className="relative w-full mx-auto overflow-visible">
                     {showMainCarousel ? (
                         <Carousel
-                            showArrows={false}
+                            showArrows={true}
                             showStatus={false}
-                            showIndicators={false}
-                            showThumbs={false}
+                            showIndicators={true}
+                            showThumbs={true}
                             autoPlay
-                            infiniteLoop
-                            interval={3000}
-                            transitionTime={300}
+                            infiniteLoop={true}
+                            interval={5000}
+                            transitionTime={1000}
                             dynamicHeight={false}
                             stopOnHover={false}
-                            className="w-full h-full"
+                            className=""
                         >
-                            {images.map((src, index) => (
-                                <div key={index} className="h-[840px] overflow-hidden relative">
-                                    <Image src={src} width={1920} height={840} className="object-cover w-full h-full filter brightness-75" alt={`Work ${index + 1}`} />
+                            {images.map((image, index) => (
+                                <div key={index} className="h-[720px] w-screen flex justify-center items-center relative">
+                                    <Image src={image.src} layout="fill" objectFit="cover" className="filter brightness-50" alt={`Work ${index + 1}`} />
+                                    <h3 className="text-white text-2xl mt-40 mr-5 z-10">{image.title}</h3>
+                                    <Link
+                                        href={image.link}
+                                        className="absolute bottom-40 mx-auto mr-5 bg-transparent border-2 border-white text-white py-2 px-4 rounded duration-200 hover:border-cyan-500 hover:text-cyan-500"
+                                    >
+                                        See More
+                                    </Link>
                                 </div>
                             ))}
                         </Carousel>
                     ) : (
-                        <div className="w-full h-full zoom-container">
+                        <div className='zoom-container'>
                             <Carousel
                                 showArrows={false}
                                 showStatus={false}
@@ -82,12 +89,12 @@ function Work({ isDarkMode }) {
                                 transitionTime={0}
                                 dynamicHeight={false}
                                 stopOnHover={false}
-                                className="w-full h-full"
+                                className=""
                                 onChange={handleSlideshowChange}
                             >
-                                {images.map((src, index) => (
-                                    <div key={index} className="h-[840px] overflow-hidden relative">
-                                        <Image src={src} width={1920} height={840} className="object-cover w-full h-full filter brightness-75 transform transition-transform duration-500" alt={`Work ${index + 1}`} />
+                                {images.map((image, index) => (
+                                    <div key={index} className="h-[720px] w-screen flex justify-center items-center relative">
+                                        <Image src={image.src} layout="fill" objectFit="cover" className="filter brightness-75 transform transition-transform duration-500" alt={`Work ${index + 1}`} />
                                     </div>
                                 ))}
                             </Carousel>
