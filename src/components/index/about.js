37bf89c74be5ee195a8b63b56fe4264e438ac3f0
AngusBlomley <125128100+AngusBlomley@users.noreply.github.com>
2024-05-19 13:50:48 +0100

    added work pages

--- src/components/index/about.js
+++ src/components/index/about.js
@@ -13,7 +13,7 @@ function About({ isDarkMode }) {
     const backgroundColor = isDarkMode ? 'var(--background-color-dark)' : 'var(--background-color-light)';
 
     return (
-        <section id="about" style={{ backgroundColor }} className="relative z-10 duration-200 flex row items-center pt-16">
+        <section id="about" style={{ backgroundColor }} className="relative z-10 duration-1000 flex row items-center pt-16" data-aos="fade-in">
             <div className="absolute inset-0">
                 <div className="absolute inset-0 bg-gradient-to-r from-black via-black/60 to-transparent opacity-100 z-10 max-md:via-black/100 max-md:to-black/50"></div>
                 <Image
@@ -27,10 +27,13 @@ function About({ isDarkMode }) {
             <div className="max-w-2xl ml-44 my-40 relative z-10 text-gray-100 max-lg:mx-auto max-lg:px-4">
                 <h2 className="text-4xl mb-8 font-semibold" data-aos="fade-up">The Person Behind the Pixels</h2>
                 <p className="text-lg mb-6 leading-relaxed" data-aos="fade-up">
-                    Hello! I'm Angus Blomley, a passionate software developer with a love for continuous learning. I began programming in May 2014 and have since mastered HTML, CSS, JavaScript, React, Python, Tailwind, MongoDB, GitHub, Vercel, Axios, and more.
+                    Hello! I'm Angus Blomley, a passionate software developer with a love for continuous learning. I began programming in May 2014 and have since mastered  HTML, CSS, JavaScript, React and many other languages, libraries and tools.
                 </p>
                 <p className="text-lg mb-6 leading-relaxed" data-aos="fade-up" data-aos-delay="200">
-                    I'm also learning Japanese to connect more deeply with my partner and their culture. As a former competitive springboard diver, I enjoy challenging outdoor activities and thrive in like-minded company. My adventurous spirit drives me to explore new places and constantly push my limits.
+                    I'ts not just programming languages I know! To connect more deeply with my partner and their culture I am continuely learning Japanese.
+                </p>
+                <p className="text-lg mb-6 leading-relaxed" data-aos="fade-up" data-aos-delay="200">
+                    As a former competitive springboard diver, I enjoy challenging outdoor activities and thrive in like-minded company. My adventurous spirit drives me to explore new places and constantly push my limits.
                 </p>
                 <p className="text-lg leading-relaxed" data-aos="fade-up" data-aos-delay="400">
                     Feel free to explore my work, and don't hesitate to reach out if you'd like to collaborate or have a chat!
--- src/components/index/work.js
+++ src/components/index/work.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -10,6 +10,8 @@ import Link from 'next/link';
 function Work({ isDarkMode }) {
     const [showMainCarousel, setShowMainCarousel] = useState(false);
     const [slideshowCompleted, setSlideshowCompleted] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(0);
+    const mainCarouselRef = useRef(null);
 
     useEffect(() => {
         AOS.init({
@@ -20,23 +22,29 @@ function Work({ isDarkMode }) {
 
     useEffect(() => {
         if (slideshowCompleted) {
+            setSelectedItem(0); // Reset selected item to the first slide
+        }
+    }, [slideshowCompleted]);
+
+    useEffect(() => {
+        if (slideshowCompleted) {
             const timer = setTimeout(() => {
                 setShowMainCarousel(true);
-            }, 500);
+            }, 0); // Set delay to 0 to make it immediate
 
             return () => clearTimeout(timer);
         }
-    }, [slideshowCompleted]);
+    }, [slideshowCompleted, selectedItem]); // Add selectedItem to the dependencies array
 
     const backgroundColor = isDarkMode ? 'var(--background-color-dark)' : 'var(--background-color-light)';
     const color = isDarkMode ? 'var(--foreground-color-dark)' : 'var(--foreground-color-light)';
 
     const images = [
-        { src: '/images/work/stringbox.png', link: '/more-info/stringbox', title: 'String Box Project' },
-        { src: '/images/work/cst.jpg', link: '/more-info/cst', title: 'Final Major Project' },
-        { src: '/images/work/befirst.png', link: '/more-info/befirst', title: 'Be First Campaign' },
-        { src: '/images/work/misuzu.png', link: '/more-info/misuzu', title: 'Misuzu Collaboration' },
-        // Add paths, links, and titles for all 10 images,
+        { src: '/images/work/stringbox.png', link: '/work/stringBox', title: 'String Box Project' },
+        { src: '/images/work/cst.jpg', link: '/work/celestialStarTracker', title: 'Final Major Project' },
+        { src: '/images/work/meetly.png', link: '/work/meetly', title: 'Meetly' },
+        { src: '/images/work/befirst.png', link: '/work/beFirst', title: 'Be First Campaign' },
+        { src: '/images/work/misuzu.png', link: '/work/misuzuPortfolio', title: 'Misuzu Collaboration' }
     ];
 
     const handleSlideshowChange = (index) => {
@@ -46,31 +54,46 @@ function Work({ isDarkMode }) {
     };
 
     return (
-        <section id="work" style={{ backgroundColor, color }} className="pt-16 relative flex justify-center items-center z-10 overflow-hidden">
-            <div className="flex justify-center items-center w-full">
+        <section
+            id="work"
+            style={{ backgroundColor, color }}
+            className="pt-16 relative flex flex-col justify-center items-center z-10 overflow-hidden h-screen"
+        >
+            <div className="flex flex-col justify-center items-center w-full">
                 <div className="relative w-full mx-auto overflow-visible">
                     {showMainCarousel ? (
-                        <div className='zoom-container'>
+                        <div className="zoom-container">
                             <Carousel
+                                ref={mainCarouselRef}
+                                selectedItem={selectedItem}
+                                onChange={(index) => setSelectedItem(index)}
                                 showArrows={true}
                                 showStatus={false}
                                 showIndicators={true}
                                 showThumbs={true}
-                                autoPlay
+                                autoPlay={true}
                                 infiniteLoop={true}
                                 interval={5000}
                                 transitionTime={1000}
                                 dynamicHeight={false}
                                 stopOnHover={false}
-                                className="relative w-full mx-auto overflow-visible"
+                                className="w-full"
                             >
                                 {images.map((image, index) => (
-                                    <div key={index} className="h-[720px] w-screen flex justify-center items-center relative">
-                                        <Image src={image.src} layout="fill" objectFit="cover" className="filter brightness-50" alt={`Work ${index + 1}`} />
-                                        <h3 className="text-white text-2xl mt-40 mr-5 z-10 max-lg:mr-0">{image.title}</h3>
+                                    <div key={index} className="h-screen flex justify-center items-center relative">
+                                        <Image
+                                            src={image.src}
+                                            layout="fill"
+                                            objectFit="cover"
+                                            className="filter brightness-50"
+                                            alt={`Work ${index + 1}`}
+                                        />
+                                        <h3 className="text-white text-2xl mt-40 mr-5 z-10 max-lg:mr-0">
+                                            {image.title}
+                                        </h3>
                                         <Link
                                             href={image.link}
-                                            className="absolute bottom-40 mx-auto mr-5 bg-transparent border-2 border-white text-white py-2 px-4 rounded duration-200 hover:border-cyan-500 hover:text-cyan-500  max-lg:mr-0"
+                                            className="absolute bottom-40 mx-auto mr-5 bg-transparent border-2 border-white text-white py-2 px-4 rounded duration-200 hover:border-cyan-500 hover:text-cyan-500 max-lg:mr-0"
                                         >
                                             See More
                                         </Link>
@@ -79,7 +102,7 @@ function Work({ isDarkMode }) {
                             </Carousel>
                         </div>
                     ) : (
-                        <div className='zoom-container'>
+                        <div className="zoom-container">
                             <Carousel
                                 showArrows={false}
                                 showStatus={false}
@@ -87,7 +110,7 @@ function Work({ isDarkMode }) {
                                 showThumbs={false}
                                 autoPlay
                                 infiniteLoop={false}
-                                interval={400}
+                                interval={200}
                                 transitionTime={0}
                                 dynamicHeight={false}
                                 stopOnHover={false}
@@ -95,8 +118,14 @@ function Work({ isDarkMode }) {
                                 onChange={handleSlideshowChange}
                             >
                                 {images.map((image, index) => (
-                                    <div key={index} className="h-[720px] w-screen flex justify-center items-center relative">
-                                        <Image src={image.src} layout="fill" objectFit="cover" className="filter brightness-75 transform transition-transform duration-500" alt={`Work ${index + 1}`} />
+                                    <div key={index} className="h-screen w-screen flex justify-center items-center relative">
+                                        <Image
+                                            src={image.src}
+                                            layout="fill"
+                                            objectFit="cover"
+                                            className="filter brightness-75 transform transition-transform duration-500"
+                                            alt={`Work ${index + 1}`}
+                                        />
                                     </div>
                                 ))}
                             </Carousel>
@@ -105,7 +134,9 @@ function Work({ isDarkMode }) {
                 </div>
             </div>
             <div className="absolute z-10 text-center" data-aos="fade-in">
-                <h2 className="text-6xl text-white font-bold mb-8 max-md:text-3xl">Creative Masterpieces</h2>
+                <h2 className="text-6xl text-white font-bold mb-8 max-md:text-3xl">
+                    Creative Masterpieces
+                </h2>
             </div>
         </section>
     );
