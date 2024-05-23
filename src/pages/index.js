65dc251aa4d37feab88307bf3965928a70dcadfc
AngusBlomley <125128100+AngusBlomley@users.noreply.github.com>
2024-05-23 14:31:20 +0100

    added title

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Education from '@/components/index/education';
 import Work from '@/components/index/work';
 import Contact from '@/components/index/contact';
 import Footer from '@/components/globals/footer';
+import Head from 'next/head';
 
 function HomePage() {
     const [isDarkMode, setIsDarkMode] = useState(false);
@@ -14,6 +15,10 @@ function HomePage() {
 
     return (
         <main className={`flex justify-center flex-col ${isDarkMode ? 'dark-mode' : ''}`}>
+            <Head>
+                <title>Angus Blomley</title>
+                <meta name="description" content="Angus Blomley's Portfolio Website" />
+            </Head>
             <Header isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
             <Main isDarkMode={isDarkMode} />
             <About isDarkMode={isDarkMode} />
